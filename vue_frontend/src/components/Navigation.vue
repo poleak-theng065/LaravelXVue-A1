486055b1825682844718ee6b8d311478e0701a5f
Phally-Chheang <phally.chheang@student.passerellesnumeriques.org>
2025-06-20 11:05:57 +0700

    created: feature/navigation #1

---
+++ vue_frontend/src/components/Navigation.vue
@@ -0,0 +1,28 @@
+<template>
+<nav class="h-16 flex items-center justify-center shadow-md bg-blue-200">
+  <div id="nav-item" class="flex gap-35">
+    <NavigationItem
+      v-for="item in navItems"
+      :key="item.label"
+      :item="item"
+      :to="item.path"
+    />
+  </div>
+</nav>
+
+</template>
+
+<script setup>
+import NavigationItem from "./NavigationItem.vue";
+import { ref } from "vue";
+
+const navItems = ref([
+  { label: "HomeView", path: "/", icon: "home" },
+  { label: "AboutView", path: "/about",icon:"3p" },
+  { label: "ContactView", path: "/contact", icon:"account_circle"},
+ 
+]);
+</script>
+
+<style lang="scss" scoped>
+</style>
